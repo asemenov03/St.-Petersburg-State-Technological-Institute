39f2c615a4907b6dc4f08e8d81ac4d2032e7c59e
asemenov03 <81237119+asemenov03@users.noreply.github.com>
2022-09-17 21:31:32 +0300

    Add files via upload

---
+++ python/лаба1/задание1.py
@@ -0,0 +1,17 @@
+a = 1234
+b = [1,2,3,4]
+c = (1,2,3,4)
+f = 123.567
+print(a*2)
+print(str(a)*2)
+print(b*2)
+print(c*2)
+b.append(1234)
+print(round(f))
+print(b[2:])
+print(c[2:])
+k = str(a).split('2')
+print(k)
+
+
+
