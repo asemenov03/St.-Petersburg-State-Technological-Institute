b4404d912ada0d8e95cc0f8f626efcc57f7f8040
asemenov03 <81237119+asemenov03@users.noreply.github.com>
2022-10-02 17:34:07 +0300

    Update program3.py

--- python/лаба2/program3.py
+++ python/лаба2/program3.py
@@ -1,7 +1,11 @@
+import time
+
 import numpy as np
+
 
 def minor(array):
     return array[0][0] * array[1][1] - array[1][0] * array[0][1]
+
 
 def division(array):
     if len(array[0]) > 2:
@@ -16,6 +20,7 @@ def division(array):
     else:
         return minor(array)
 
+
 def trans_m(m_m):
     m = m_m
     trans_m = [[0] * int(3) for i in range(3)]
@@ -24,6 +29,7 @@ def trans_m(m_m):
             trans_m[j][i] = m_m[i][j]
     return trans_m
 
+
 def inputt():
     m = [[0] * 3 for i in range(3)]
     for i in range(len(m)):
@@ -31,33 +37,40 @@ def inputt():
             m[i][j] = int(input('Введите элемент матрицы: '))
     return m
 
+
 def bez_numpy(m):
+    start = time.perf_counter()
     m1_new = [[0] * 3 for k in range(3)]
     for k1 in range(len(m)):
         for k2 in range(len(m)):
             m_new = []
             for i in range(len(m)):
                 for j in range(len(m)):
-                    if k1!=i and k2!=j:
+                    if k1 != i and k2 != j:
                         m_new.append(m[i][j])
-            m_new = [[m_new[0],m_new[1]],[m_new[2],m_new[3]]]
-            m1_new[k1][k2] = (-1)**(k1+k2+2)*minor(m_new)
+            m_new = [[m_new[0], m_new[1]], [m_new[2], m_new[3]]]
+            m1_new[k1][k2] = (-1) ** (k1 + k2 + 2) * minor(m_new)
 
     det = division(m)
     m1_new = trans_m(m1_new)
     for i in range(len(m1_new)):
         for j in range(len(m1_new)):
-            m1_new[i][j] = 1/det * m1_new[i][j]
+            m1_new[i][j] = 1 / det * m1_new[i][j]
     print('Результат:')
     for i in range(len(m1_new)):
         print(*m1_new[i])
+    print(f'{time.perf_counter() - start} секунд')
+
 
 def s_numpy(m):
+    start = time.perf_counter()
     m = np.array(m)
     m = np.linalg.inv(m)
     print('Результат:')
     for i in range(len(m)):
         print(*m[i])
+    print(f'{time.perf_counter() - start} секунд')
+
 
 f = inputt()
 print('Решение без numpy')
