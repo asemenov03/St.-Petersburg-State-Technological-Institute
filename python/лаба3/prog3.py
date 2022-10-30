2808f59d46d9f130a1584c51ed7a39095c73ea54
asemenov03 <81237119+asemenov03@users.noreply.github.com>
2022-10-30 17:01:27 +0300

    Update prog3.py

--- python/лаба3/prog3.py
+++ python/лаба3/prog3.py
@@ -1,1 +1,39 @@
-print('')
+import time
+import random
+import math
+
+
+def o_1(n=1):
+    start_time = time.perf_counter()
+    print("--- %s seconds ---" % (time.perf_counter() - start_time))
+    return 1
+
+def logn(n):
+    start_time = time.perf_counter()
+    count=0
+    for i in range(round(math.log2(n))):
+        count+=1
+    print("--- %s seconds ---" % (time.perf_counter() - start_time))
+    return  count
+
+def n2(n):
+    start_time = time.perf_counter()
+    count =0
+    for i in range(n):
+        for j in range(n):
+            count+=1
+    print("--- %s seconds ---" % (time.perf_counter() - start_time))
+    return count
+
+def o_2n(n):
+    start_time = time.perf_counter()
+    count=0
+    for i in range(2**n):
+        count+=1
+    print("--- %s seconds ---" % (time.perf_counter() - start_time))
+    return  count
+
+o_1()
+logn(6)
+n2(6)
+o_2n(6)
