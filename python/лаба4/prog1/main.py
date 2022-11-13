b72693b21b2b15304bbadd266e17c50ab71d0b82
asemenov03 <81237119+asemenov03@users.noreply.github.com>
2022-11-13 15:40:05 +0300

    Update main.py

--- python/лаба4/prog1/main.py
+++ python/лаба4/prog1/main.py
@@ -1,1 +1,14 @@
-import math
+import fanc
+import time
+
+i = int(input('Выберете сортировку: 1 - быстрая сортировка, 2 - сортировка расческой - '))
+if i == 1:
+    my_list = list(map(int,input("Ввод списка: ").split()))
+    start = time.time() * 1000
+    print(fanc.f(my_list))
+    print(f'Время в миллисекундах: {time.time() * 1000 - start}')
+elif i == 2:
+    my_list = list(map(int,input("Ввод списка: ").split()))
+    start = time.time() * 1000
+    print(fanc.greben(my_list))
+    print(f'Время в миллисекундах: {time.time() * 1000 - start}')
